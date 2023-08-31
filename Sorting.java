bff5fca60e78fabce2a1235f5a3ce3b0c66ad50c
Austin Ransford <aransford2024@chadwickschool.org>
2023-08-31 09:16:43 -0700

    merge sort was started

--- Sorting.java
+++ Sorting.java
@@ -90,6 +90,89 @@ public class Sorting
             System.out.println(num);
         }
     }
-    
-    
+    public void insertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.length; i++)
+        {
+            for(int j = i - 1; j >= 0; j--)
+            {
+                int temp = arr[i];
+                if (temp < arr[j]) 
+                {
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+     public void insertionTest()
+    {
+        int[] arr = {70, 5, 29, 54, -4, 89, 100, 0, 0 -100, 5};
+        for (int num: arr)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println("");
+        insertionSort(arr);
+        for (int num: arr)
+        {
+            System.out.println(num);
+        }
+    }
+    public void mergeSort(int[] arr)
+    {
+        mergeSorting(0, arr.length, arr);
+        
+    }
+    public void mergeSorting(int start, int end, int[] arr)
+    {
+        int mid = (end + start) / 2;
+        if(start == end)
+        {
+            return;
+        }
+        else
+        {
+            mergeSorting(start, mid, arr);
+            mergeSorting(mid +1, end, arr);
+            merge(start, mid, end, arr);
+        }
+    }
+    public void merge(int start, int mid, int end, int[] arr)
+    {
+        int i = start;
+        int j = mid; 
+        int[] mergedArray = arr;
+        int location = start;
+        while (i < mid && j <= end)
+        {
+            if (arr[i] < arr[j])
+            {
+                mergedArray[location] = arr[i];
+                i++;
+                location++;
+            }
+            else
+            {
+                mergedArray[location] = arr[j];
+                j++;
+                location++;
+            }
+        }
+        arr = mergedArray; 
+    }
+    public void mergeTest()
+    {
+        int[] arr = {70, 5, 29, 54, -4, 89, 100, 0, 0 -100, 5};
+        for (int num: arr)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println("");
+        mergeSort(arr);
+        for (int num: arr)
+        {
+            System.out.println(num);
+        }
+    }
 }
